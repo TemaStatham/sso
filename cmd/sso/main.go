edaf927a8cdd2efc1c2a6c9bcc5b78fb0c9b3c5c
Karatist <skammoshenik@gmail.com>
2024-03-03 12:34:03 +0300

    добавил логгер

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/TemaStatham/sso/internal/config"
 )
 
 // go run ./cmd/sso/main.go --config="./config/config.yaml"
 
+const (
+	envLocal="local"
+	envDev="dev"
+	envProd="prod"
+)
+
 func main() {
 	cfg := config.MustLoad()
-	fmt.Print(cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("starting application", slog.Any("cfg", cfg))
+	log.Debug("debug message")
+	log.Error("error mesage")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch (env) {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+			log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
